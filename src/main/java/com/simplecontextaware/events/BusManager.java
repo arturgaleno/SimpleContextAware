03f5398b562af546209316484abd50425964cda8
artur <artur@artur-pc>
2013-12-16 09:40:15 -0300

    Read Me

--- src/main/java/com/simplecontextaware/events/BusManager.java
+++ src/main/java/com/simplecontextaware/events/BusManager.java
@@ -4,15 +4,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.squareup.otto.Bus;
+import com.squareup.otto.ThreadEnforcer;
 
 
 public class BusManager {
 	
 	private static final Bus bus = new Bus();
 	
+	private static final Bus busAnyThread = new Bus(ThreadEnforcer.NONE);
+	
 	private static ArrayList<Object> producers = new ArrayList<Object>();
 	
 	private static ArrayList<Object> receivers = new ArrayList<Object>();
+	
+	private static ArrayList<Object> producersAnyThread = new ArrayList<Object>();
+	
+	private static ArrayList<Object> receiversAnyThread = new ArrayList<Object>();
 	
 	public BusManager() {
 	}
@@ -33,6 +40,24 @@ public class BusManager {
 	
 	public static Bus getBus() {
 		return bus;
+	}
+	
+	public static void registerAnyThreadProducer(Object obj) {
+		if (!verifyIfExists(producersAnyThread,obj)) {
+			busAnyThread.register(obj);
+			producersAnyThread.add(obj);
+		}
+	}
+	
+	public static void registerAnyThreadReceiver(Object obj) {
+		if (!verifyIfExists(receiversAnyThread,obj)) {
+			busAnyThread.register(obj);
+			receiversAnyThread.add(obj);
+		}
+	}
+	
+	public static Bus getAnyThreadBus() {
+		return busAnyThread;
 	}
 	
 	private static boolean verifyIfExists(List<Object> parent, Object object) {
